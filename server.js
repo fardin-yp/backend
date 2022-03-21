0d96e1e434c6efec50ae855096ce69a9ce202c12
mr-Alarak <82238611+fardin-yp@users.noreply.github.com>
2022-03-21 18:09:29 +0330

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = https.createServer(app);
 const MongoStore = require('connect-mongo');
 const multiparty = require('connect-multiparty');
 const io = require("socket.io")(server ,{
-   cors:{origin:"https://frontend-fardin-yp.vercel.app"}
+   cors:{origin:"https://dream-web.vercel.app/"}
 });
 const bodyParserErrorHandler = require('express-body-parser-error-handler')
 
@@ -30,7 +30,7 @@ app.use(bodyParserErrorHandler());
    app.use(express.static("uploads"));
    app.use(cookieparser())
    app.use(cors({
-   origin:["https://frontend-fardin-yp.vercel.app"],
+   origin:["https://frontend-fardin-yp.vercel.app","https://dream-web.vercel.app/"],
    credentials:true,
    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
    allowedHeaders: ["Content-Type", "Authorization"],
